dd59e84701bc952f21c82508440314ba6ac7dfba
Daniel Merida B <daniel.meridab@niceincontact.com>
2019-07-24 12:30:52 -0400

    clean project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import CardHeader from '@material-ui/core/CardHeader';
 
-function App() {
-  return (
-    <div className="App">
-      <h3>Title</h3>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    const title = "WELLCOME TO SSP-SERVICE"
+    return (
+      <div>
+        <CardHeader title={title} style={{ textAlign: "center"}}/>
+      </div>
+    );
+  }
 }
 
 export default App;
